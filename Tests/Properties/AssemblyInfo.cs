746cbf63131f4d9d233ae87474bb4c1e4ec713c4
shlok57 <shlok.gandhi@gmail.com>
2018-08-26 01:44:31 -0400

    Added Test Project

---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("10754c51-8fdd-4606-be68-a572d31a94a6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Shlok.DBProviders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shlok.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		[TestCategory("Unit")]
+		public void FunctionTest()
+		{
+			int value = 5;
+			Assert.AreEqual(value * value, DBProvider.Function(value));
+		}
+	}
+}
